d02ab8f6e308df4c9d80f0fc3e15f351935ed5b3
Andre Luiz <97361918+AndreLuizBoanergers@users.noreply.github.com>
2024-08-21 21:49:50 -0300

    Add files via upload
    nao a inferencia de tipo  int   string ele auto entende  entre aspa  string   e por ai vai

---
+++ interferencia.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		var nome string = "Andre"
+		var idade int = 37
+		var versao float32 = 2.2
+		fmt.Println("Ola sr(a).", nome, "sua idade é", idade)
+		fmt.Println("Este program,a esta na versao", versao)
+	*/
+	var nome = "Andre"
+	var idade = 37
+	var versao = 2.2
+	fmt.Println("Ola sr(a).", nome, "sua idade é", idade)
+	fmt.Println("Este program,a esta na versao", versao)
+}
