36af70e18c331e925697fb6ec8f057eba861d9fb
Andre Luiz <97361918+AndreLuizBoanergers@users.noreply.github.com>
2024-08-23 17:19:22 -0300

    Add files via upload

--- temperatura.go
+++ temperatura.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 //ponto de ebulicao da gua em Fahrenhe8it
-const ebulicaoF float64 = 212.0
+const ebulicaoF = 212.0
 
 func main() {
-	var tempF float64 = ebulicaoF
-	var tempC float64 = (tempF - 32) * 5 / 9 //Conversao de F para C
-	fmt.Println("A temperatura de ebulicao em  garus F = ", tempF)
-	fmt.Println("A temperatura de ebulicao em graus C = ", tempC)
+	tempF := ebulicaoF
+	tempC := (tempF - 32) * 5 / 9 //Conversao de F para C
+	/*
+		fmt.Println("A temperatura de ebulicao em  garus F = ", tempF)
+		fmt.Println("A temperatura de ebulicao em graus C = ", tempC)
+	*/
+	fmt.Printf("A temperatura de ebulicao em f e %g ,e a temperatura em C e %g", tempF, tempC)
 }
